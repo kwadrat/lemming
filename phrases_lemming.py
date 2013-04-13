db510f16e738f141ccb80f3d5098b1273171c533
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-13 11:16:13 +0200

    Update common_phrases

--- phrases_lemming.py
+++ phrases_lemming.py
@@ -5,4 +5,8 @@ common_phrases = {
     '1': '%s - copy',
     '2': '%s - switch to copy',
     '3': '%s - remove original',
+    '4': 'Add %s',
+    '5': 'Use %s',
+    '6': 'Remove %s',
+    '7': 'Check %s',
 }
